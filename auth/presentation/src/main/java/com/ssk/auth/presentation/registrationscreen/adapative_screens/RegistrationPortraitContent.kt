9fc8e3a9585ce903969f0f3cf52223174a132c41
Seeker1805 <kulkarnisameer1809@gmail.com>
2025-06-16 12:19:04 -0400

    Clean up

--- auth/presentation/src/main/java/com/ssk/auth/presentation/registrationscreen/adapative_screens/RegistrationPortraitContent.kt
+++ auth/presentation/src/main/java/com/ssk/auth/presentation/registrationscreen/adapative_screens/RegistrationPortraitContent.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -58,7 +60,8 @@ fun RegistrationPortraitContent(
                     )
                 )
                 .fillMaxHeight()
-                .padding(horizontal = 16.dp, vertical = 16.dp),
+                .padding(horizontal = 16.dp, vertical = 16.dp)
+                .verticalScroll(rememberScrollState())
         ) {
             Text(
                 text = "Create account",
--- core/data/src/main/java/com/ssk/core/data/models/Constants.kt
+++ core/data/src/main/java/com/ssk/core/data/models/Constants.kt
@@ -2,5 +2,4 @@ package com.ssk.core.data.models
 
 object Constants {
     const val SAM_EMAIL = "babakul18@gmail.com"
-    const val starChar = '\u002A'
 }
